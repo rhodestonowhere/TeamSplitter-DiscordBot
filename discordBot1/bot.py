497d3cd06d0cefb58a9bc269028651e355332a08
Jeffery Liang <jliang17@mail.sfsu.edu>
2022-10-10 14:32:38 -0700

    housekeeping, added comments

--- discordBot1/bot.py
+++ discordBot1/bot.py
@@ -71,9 +71,7 @@ async def fair_split(context):
         pair = tuple.split(",") #split tuple using "," delimiter to seperate player name and skill
         playerDict.setdefault(pair[0], pair[1]) #add player's name and according skill to dictionary
 
-
-    #Various algorithms for creating balanced teams:
-    #Method 1: split teams based on score total
+    #variable delcaration
     team1 = []
     team2 = []
     team1skill = 0
@@ -141,34 +139,34 @@ async def fair_split_2(context):
             add to team with highest average skill
         if either team is has reached team capacity then put remaining players into other team
     """
-    team1 = [] #list declaration
+    team1 = [] 
     team2 = []
     keys = list(playerDict.keys()) #get list of all keys in the dictionary as list
     team1.insert(0, keys[0]) #insert first and second elements of list into teams
     team2.insert(0, keys[1])
     team1TotalSkill = playerDict[team1[0]] #get skill of initial players
-    keys.pop(0)
+    keys.pop(0) #remove player from dictionary and list
     playerDict.pop(team1[0])
     team2TotalSkill = playerDict[team2[0]]
     keys.pop(1)
     playerDict.pop(team2[0])
 
     
-    for player in playerDict:
+    for player in playerDict: #for every player in playerDict
         print('///////////////////////////////////////////')
         print("|Loop Start| Global Average is: {}".format(globalAvg))
         print('///////////////////////////////////////////')
-        team1Avg = team1TotalSkill / len(team1) 
+        team1Avg = team1TotalSkill / len(team1) #calculate the average skill of both teams
         team2Avg = team2TotalSkill / len(team2)
         print("Team 1 average is: {}".format(team1Avg))
         print("Team 2 average is: {}".format(team2Avg))
 
-        if len(team1) == (len(playerDict)+2) / 2:
+        if len(team1) == (len(playerDict)+2) / 2: #check if the length of either team has reached roughly half of the total players
             print("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::")
             print("Team 1 has reached capacity! Placing remaining members in Team 2.")
             print("player name is: {}".format(player))
-            team2.insert(0, player)
-            team2TotalSkill += playerDict[player]
+            team2.insert(0, player) #if so, then add player to opposite team
+            team2TotalSkill += playerDict[player] #add skill to team's total skill
             continue
 
         elif len(team2) == (len(playerDict)+2) / 2:       
@@ -179,44 +177,36 @@ async def fair_split_2(context):
             team1TotalSkill += playerDict[player]
             continue
 
-        if team1Avg > team2Avg:
+        if team1Avg > team2Avg: #in the case that team 2 is lower skilled team
             print("Low Average Team is: Team 2")
-            lowAvgTeam = team2
-            lowTotalSkill = team2TotalSkill
-            highAvgTeam = team1
-            highTotalSkill = team1TotalSkill
-            if playerDict[player] > globalAvg:
+            if playerDict[player] > globalAvg: #if player skill is higher than the global average
                 print("player skill: higher than globalAvg")
                 print("player name is: {}".format(player))
                 print("player skill is: {}".format(playerDict[player]))
-                lowAvgTeam.insert(0, player)
+                team2.insert(0, player) #add player to lower skilled team
                 team2TotalSkill += playerDict[player]
                 print("Current Team 1: {}".format(team1))
                 print("Current Team 1 Skill: {}".format(team1TotalSkill))
                 print("Current Team2 : {}".format(team2))
                 print("Current Team 2 Skill: {}".format(team2TotalSkill))
 
-            elif playerDict[player] < globalAvg:
+            elif playerDict[player] < globalAvg: #player skill is less than average
                 print("player skill: lower than globalAvg")
                 print("player name is: {}".format(player))
                 print("player skill is: {}".format(playerDict[player]))
-                highAvgTeam.insert(0, player)
+                team1.insert(0, player) #add to higher skilled team
                 team1TotalSkill += playerDict[player]
                 print("Current Team 1: {}".format(team1))
                 print("Current Team 1 Skill: {}".format(team1TotalSkill))
                 print("Current Team2 : {}".format(team2))
                 print("Current Team 2 Skill: {}".format(team2TotalSkill))      
-        else:
+        else: #in the case that team 1 is the lower skilled team
             print("Low Average Team is: Team 1")
-            lowAvgTeam = team1
-            lowTotalSkill = team1TotalSkill
-            highAvgTeam = team2
-            highTotalSkill = team2TotalSkill
             if playerDict[player] > globalAvg:
                 print("player skill: higher than globalAvg")
                 print("player name is: {}".format(player))
                 print("player skill is: {}".format(playerDict[player]))
-                lowAvgTeam.insert(0, player)
+                team1.insert(0, player)
                 team1TotalSkill += playerDict[player]
                 print("Current Team 1: {}".format(team1))
                 print("Current Team 1 Skill: {}".format(team1TotalSkill))
@@ -227,7 +217,7 @@ async def fair_split_2(context):
                 print("player skill: lower than globalAvg")
                 print("player name is: {}".format(player))
                 print("player skill is: {}".format(playerDict[player]))
-                highAvgTeam.insert(0, player)
+                team2.insert(0, player)
                 team2TotalSkill += playerDict[player]
                 print("Current Team 1: {}".format(team1))
                 print("Current Team 1 Skill: {}".format(team1TotalSkill))
